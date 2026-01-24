26d95b58b0e387c738e474d196a175c8b76a455c
myj9713 <myj9713@gmail.com>
2026-01-25 05:16:58 +0900

    5주차 과제 회고록 작성, js 파일 수정

--- JavaScript/5w-homework/tab-menu_practice/assets/scripts/main.js
+++ JavaScript/5w-homework/tab-menu_practice/assets/scripts/main.js
@@ -1,5 +1,3 @@
-const dashboard = document.querySelector('.dashboard')
-// console.log(dashboard)
 const tabButtons = document.querySelectorAll('.tab-nav__button')
 // console.log(tabButtons)
 const tabPanels = document.querySelectorAll('.tab-panel')
@@ -8,12 +6,14 @@ const tabPanels = document.querySelectorAll('.tab-panel')
 tabButtons.forEach((button) => {
   button.addEventListener('click', () => {
     const targetId = button.getAttribute('data-target')
+    // console.log('연결된 타겟 ID:', targetId)
 
     // 모든 버튼 및 패널 초기화
     tabButtons.forEach(btn => {
       btn.classList.remove('tab-nav__button--active')
       btn.setAttribute('aria-selected', 'false')
     })
+
     tabPanels.forEach(panel => {
       panel.classList.remove('tab-panel--active')
     })
@@ -22,10 +22,10 @@ tabButtons.forEach((button) => {
     button.classList.add('tab-nav__button--active')
     button.setAttribute('aria-selected', 'true')
     const targetPanel = document.getElementById(targetId)
+    // console.log('타겟 패널:', targetPanel)
+
     if (targetPanel) {
       targetPanel.classList.add('tab-panel--active')
     }
-    
-    // console.log('Active Panel:', targetId)
   })
 })
